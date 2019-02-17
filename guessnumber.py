3b4cbd72974aba28af4338cf15757b614501bf38
Oscar <oscar6292@gmail.com>
2019-02-17 13:14:27 +0800

    v.2

--- guessnumber.py
+++ guessnumber.py
@@ -6,7 +6,9 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
@@ -16,3 +18,5 @@ while True:
 		print('你猜得比答案還小')
 	else:
 		print('你猜得比答案還大')
+	print('這是你猜的第', count,'次')
+
